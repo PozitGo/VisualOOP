d68f786faf1d7684c4d93e517201d122f8be3f33
PozitGo <pozitgo2005@gmail.com>
2023-09-06 11:17:46 +0300

    Add PR1

--- PR1/Form1.cs
+++ PR1/Form1.cs
@@ -2,9 +2,25 @@ namespace PR1
 {
     public partial class Form1 : Form
     {
+        public static int number = 1;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (number <= 9)
+            {
+                listBox1.Items.Add($"{number++} קטסכמ");
+            }
+            else
+            {
+                listBox1.Items.Add("Öטפנ במכüרו םוע");
+                button1.Enabled = false;
+            }
+
+            textBox1.Text = (number - 1) < 9 ? (number - 1).ToString() : "Öטפנ במכüרו םוע";
         }
     }
 }
